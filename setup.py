dc1799267fb2b025993a1d82d0f3e18adffa6038
derwolfe <chriswwolfe@gmail.com>
2015-08-12 09:25:58 -0500

    on build, make a new __init__.py file where ever the zope path is

--- setup.py
+++ setup.py
@@ -1,5 +1,13 @@
 import sys
+import os
 from cx_Freeze import setup, Executable
+
+# because of how namespace packages work, cx-freeze isn't finding zope.interface
+# the following will import it, find the path of zope, and add a new empty
+# file name _init__.py at the /site-packages/python2.7/zope path.
+import zope
+path = zope.__path__[0]
+open(os.path.join(path, '__init__.py'), 'wb')  # create __init__.py file
 
 # Dependencies are automatically detected, but it might need fine tuning.
 build_exe_options = {"packages": ["zope.interface"]}
